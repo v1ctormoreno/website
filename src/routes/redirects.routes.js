1d33902f5842b21d73e1cd9f8d029f7494aff25b
Victor Moreno <victormorenotin@gmail.com>
2020-05-09 22:53:30 +0200

    changed business stuff

--- src/routes/redirects.routes.js
+++ src/routes/redirects.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 //const pool = require('../database');
 
 router.get('/controlaccesos', (req, res) => {
-    res.redirect('/projects');
+    res.redirect('https://github.com/v1ctormoreno/controlaccesos_arduino');
 })
 
 

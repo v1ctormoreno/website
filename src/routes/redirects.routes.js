5863ab2bed92b12560ac540273b890f8d7337b0a
Victor Moreno <victormorenotin@gmail.com>
2020-05-18 18:49:09 +0200

    added new redirect

--- src/routes/redirects.routes.js
+++ src/routes/redirects.routes.js
@@ -4,6 +4,9 @@ const router = require('express').Router();
 router.get('/controlaccesos', (req, res) => {
     res.redirect('https://github.com/v1ctormoreno/controlaccesos_arduino');
 })
+router.get('/referidodhosting', (req, res) => {
+    res.redirect('https://dinahosting.com/?affref=5EC2BB8568FF7');
+})
 
 
 module.exports = router;

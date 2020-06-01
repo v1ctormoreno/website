cd67479682cc4470bd3f7a590aca3d49bdde6caa
Victor Moreno <victormorenotin@gmail.com>
2020-06-01 13:44:01 +0200

    modified route

--- src/routes/redirects.routes.js
+++ src/routes/redirects.routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 //const pool = require('../database');
 
-router.get('/controlaccesos', (req, res) => {
-    res.redirect('https://github.com/v1ctormoreno/controlaccesos_arduino');
-})
+router.get("/controlaccessos", (req, res) => {
+  res.redirect("https://github.com/v1ctormoreno/controlaccesos_arduino");
+});
 router.get('/dhosting', (req, res) => {
     res.redirect('https://dinahosting.com/?affref=5EC2BB8568FF7&utm_affcmpgn=webpersonal');
 })
